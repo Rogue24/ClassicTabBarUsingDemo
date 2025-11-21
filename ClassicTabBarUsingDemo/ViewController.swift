22fdabd864ea9a666ce3e7deea2ffa40c6afea4b
Rogue.Ping <zhoujianping24@hotmail.com>
2025-11-21 11:18:42 +0800

    first upload

---
+++ ClassicTabBarUsingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassicTabBarUsingDemo
+//
+//  Created by aa on 2025/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
